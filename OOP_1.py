54c40dff1460f7da0c13e0fbe4a1615a58a6b46e
Ekaterina Malysheva <ekaterinamalysheva@iMac-Ekaterina.local>
2021-05-03 12:48:48 +0300

    OOP1

---
+++ OOP_1.py
@@ -0,0 +1,52 @@
+
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return "Ошибка"
+
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades)
+
+
+class Lecturer(Mentor):
+    pass
+
+
+class Reviewer(Mentor):
+    pass
+
+
+Lecture1 = Lecturer('Some', 'Buddy')
+print(Lecture1.name)
+print(Lecture1.surname)
+print(Lecture1.courses_attached)
